84e60deef980a9b76e1726718c9145fe6723fcef
Christiaan Steenkist <chnstone13@gmail.com>
2017-12-16 16:08:16 +0100

    increased methane hunger

--- spacecompaneer.js
+++ spacecompaneer.js
@@ -19,6 +19,7 @@ var Script = (function() {
 		
 		if (Script.fuelTier >= 1) {Script.space.explorePlace(Script.space); Script.cons.addCons(Script.cons, "rocketFuel", 2);}
 		if (Script.phase >= 2) {Script.cons.addCons(Script.cons, "oil", 5); Script.cons.addCons(Script.cons, "wood", 100);}
+		if (Script.phase >= 3) {Script.cons.addCons(Script.cons, "methane", 100);}
 		if (Script.phase >= 4) {Script.wonders.update(Script.wonders); Script.cons.addCons(Script.cons, "lunarite", 100); Script.cons.addCons(Script.cons, "titanium", 100);}
 		if (Script.data.producerColumn === "lunarite") {Script.cons.addCons(Script.cons, "lunarite", 10);}
 		if (Script.data.producerColumn === "methane") {Script.cons.addCons(Script.cons, "lunarite", 20);}
