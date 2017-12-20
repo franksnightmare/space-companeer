5b70ed6b29a02cf3661b9e18fb3b7a638c3888db
Christiaan Steenkist <chnstone13@gmail.com>
2017-12-21 00:06:39 +0100

    fixed a wrong variable name from copypastaing

--- data.js
+++ data.js
@@ -143,7 +143,7 @@ Script.data = (function(){
 		}
 		
 		for (key in building.cons) {
-			var drain = building.cons[resource] / 1000 + result.time / 2400;
+			var drain = building.cons[key] / 1000 + result.time / 2400;
 			if (getProduction(key) < building.cons[key] * (1.1 + 1.4 * Math.pow(2, -drain))) {result.canBuild = false;}
 		}
 		
