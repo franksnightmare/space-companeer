b9639de719fc1d4e5ac36e6f4f5ac970e6abd6a3
Christiaan Steenkist <chnstone13@gmail.com>
2017-12-28 01:42:08 +0100

    it would also be nice to load the dyson file

--- spacecompaneer.js
+++ spacecompaneer.js
@@ -93,6 +93,7 @@ var Script = (function() {
 		document.head.appendChild(document.createElement('script')).src = SC_base + 'cost.js';
 		document.head.appendChild(document.createElement('script')).src = SC_base + 'consumption.js';
 		document.head.appendChild(document.createElement('script')).src = SC_base + 'energy.js';
+		document.head.appendChild(document.createElement('script')).src = SC_base + 'dyson.js';
 	};
 	
 	return instance;
