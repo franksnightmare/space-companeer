8c194b4d269155094c2dce66f82932ee2b4082f5
Christiaan Steenkist <chnstone13@gmail.com>
2018-01-03 18:22:36 +0100

    another fix for wonder rush + energy producer switch because faster wonders

--- space.js
+++ space.js
@@ -12,7 +12,7 @@ Script.space = (function(){
 	instance.milestones["AsteroidBelt"] = {requirements:{"rocketFuel":200}, available:false, done:false, unlocks:["WonderStation", "Jupiter", "Saturn", "Pluto", "KuiperBelt"], consequences:function(){Script.data.unlockResource(Script.data, "silver");}};
 	instance.milestones["WonderStation"] = {requirements:{"rocketFuel":500}, available:false, done:false, unlocks:[], consequences:function(){Script.phase = 4;}};
 	instance.milestones["Jupiter"] = {requirements:{"rocketFuel":1000}, available:false, done:false, unlocks:[], consequences:function(){Script.data.unlockResource(Script.data, "hydrogen");}};
-	instance.milestones["Saturn"] = {requirements:{"rocketFuel":2000}, available:false, done:false, unlocks:[], consequences:function(){Script.data.unlockResource(Script.data, "helium"); Script.energyTier = 4; Script.science.techs["efficiencyResearch"].available = true;}};
+	instance.milestones["Saturn"] = {requirements:{"rocketFuel":2000}, available:false, done:false, unlocks:[], consequences:function(){Script.data.unlockResource(Script.data, "helium"); Script.energyTier = 6; Script.science.techs["efficiencyResearch"].available = true;}};
 	instance.milestones["Pluto"] = {requirements:{"rocketFuel":5000}, available:false, done:false, unlocks:[], consequences:function(){Script.data.unlockResource(Script.data, "ice");}};
 	instance.milestones["KuiperBelt"] = {requirements:{"rocketFuel":6000}, available:false, done:false, unlocks:["SolCenter"], consequences:function(){}};
 	instance.milestones["SolCenter"] = {requirements:{"rocketFuel":7000}, available:false, done:false, unlocks:[], consequences:function(){Script.phase = 5; Script.wonders.wonders["plasma"].available = true;}};
--- wonder.js
+++ wonder.js
@@ -5,9 +5,9 @@ Script.wonders = (function(){
 	
 	instance.wonders = {};
 	instance.wonders["precious1"] = {cost:{"gem":10000, "gold":5000, "silver":7500}, available:true, done:false, mk:achievePreciousWonder, unlocks:["precious2"], consequences:function(){}, skip:function(){return Game.statistics.entries.wondersBuilt.value >= 1;}};
-	instance.wonders["precious2"] = {cost:{"gem":30000, "gold":10000, "silver":20000}, available:false, done:false, mk:activatePreciousWonder, unlocks:[], consequences:function(){Script.wonders.wonders["energetic1"].available = true; Script.data.unlockResource(Script.data, "uranium"); Script.energyTier = 5;}, skip:function(){return Game.statistics.entries.wondersActivated.value >= 1;}};
+	instance.wonders["precious2"] = {cost:{"gem":30000, "gold":10000, "silver":20000}, available:false, done:false, mk:activatePreciousWonder, unlocks:[], consequences:function(){Script.wonders.wonders["energetic1"].available = true; Script.data.unlockResource(Script.data, "uranium"); Script.energyTier = 4;}, skip:function(){return Game.statistics.entries.wondersActivated.value >= 1;}};
 	instance.wonders["energetic1"] = {cost:{"wood":10000, "charcoal":5000, "uranium":200}, available:false, done:false, mk:achieveEnergeticWonder, unlocks:["energetic2", "tech1"], consequences:function(){}, skip:function(){return Game.statistics.entries.wondersBuilt.value >= 2;}};
-	instance.wonders["energetic2"] = {cost:{"wood":30000, "charcoal":15000, "uranium":500}, available:false, done:false, mk:activateEnergeticWonder, unlocks:[], consequences:function(){Script.data.unlockResource(Script.data, "lava"); Script.energyTier = 6;}, skip:function(){return Game.statistics.entries.wondersActivated.value >= 2;}};
+	instance.wonders["energetic2"] = {cost:{"wood":30000, "charcoal":15000, "uranium":500}, available:false, done:false, mk:activateEnergeticWonder, unlocks:[], consequences:function(){Script.data.unlockResource(Script.data, "lava"); Script.energyTier = 5;}, skip:function(){return Game.statistics.entries.wondersActivated.value >= 2;}};
 	instance.wonders["tech1"] = {cost:{"gem":40000, "silicon":30000, "gold":18000}, available:false, done:false, mk:achieveTechWonder, unlocks:["tech2"], consequences:function(){}, skip:function(){return Game.statistics.entries.wondersBuilt.value >= 3;}};
 	instance.wonders["tech2"] = {cost:{"gem":60000, "silicon":50000, "gold":30000}, available:false, done:false, mk:activateTechWonder, unlocks:[], consequences:function(){Script.machineTier = 3;}, skip:function(){return Game.statistics.entries.wondersActivated.value >= 3;}};
 	instance.wonders["plasma"] = {cost:{"hydrogen":1500, "uranium":1500, "oil":15000, "wood":15000}, available:false, done:false, mk:unlockPlasmaResearch, unlocks:[], consequences:function(){Script.science.techs["unlockPlasma"].available = true;}, skip:function(){return Game.tech.isPurchased("unlockPlasma");}};
@@ -41,8 +41,8 @@ Script.wonders = (function(){
 					var production = getProduction(key);
 					var cost = wonder.cost[key];
 					if (production <= 0) {rush = false;}
-					if (production * 240 < (cost - getResource(resource))) {rush = false;}
-					if (cost > getStorage(resource)) {rush = false;}
+					if (production * 240 < (cost - getResource(key))) {rush = false;}
+					if (cost > getStorage(key)) {rush = false;}
 				}
 				if (rush)
 				{
