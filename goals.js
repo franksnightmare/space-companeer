0cc56c193f0f47c3bfab26b08663716c95a2157a
Christiaan Steenkist <chnstone13@gmail.com>
2018-01-04 13:03:03 +0100

    another small modification

--- goals.js
+++ goals.js
@@ -30,7 +30,7 @@ Script.goals = (function(){
 			
 			if (Script.cost.counter[key])
 			{
-				request = 1.6 * Script.data.maxProd * Script.cost.balance[key];
+				request = 1.4 * Script.data.maxProd * Script.cost.balance[key];
 				if (Script.phase <= 3) {request *= 1.4;}
 				if (request > self[key].amount) {self[key].amount = request; self[key].type = "cost";}
 			}
